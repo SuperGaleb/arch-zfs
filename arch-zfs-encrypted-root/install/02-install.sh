18b8a7a87715bf9ca9c7131da054ad7304f57a19
Nenad <78131534+SuperGaleb@users.noreply.github.com>
2023-07-13 11:32:39 +1000

    Update 02-install.sh

--- arch-zfs-encrypted-root/install/02-install.sh
+++ arch-zfs-encrypted-root/install/02-install.sh
@@ -251,14 +251,14 @@ Kernel:
 EOF
 
 # Set cmdline
-zfs set org.zfsbootmenu:commandline="rw quiet nowatchdog rd.vconsole.keymap=fr" zroot/ROOT/"$root_dataset"
+zfs set org.zfsbootmenu:commandline="rw quiet nowatchdog rd.vconsole.keymap=us" zroot/ROOT/"$root_dataset"
 
 # Generate ZBM
 print 'Generate zbm'
 arch-chroot /mnt /bin/bash -xe <<"EOF"
 
   # Export locale
-  export LANG="fr_FR.UTF-8"
+  export LANG="en_AU.UTF-8"
 
   # Generate zfsbootmenu
   generate-zbm
